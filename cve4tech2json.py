02ac544a4a29b06321619ef2bc97ef214e55781e
JarVarBin <93614373+jarvarbin@users.noreply.github.com>
2022-12-04 14:26:15 +0100

    Create cve4tech2json.py

---
+++ cve4tech2json.py
@@ -0,0 +1,52 @@
+# Import the necessary modules
+import json
+import requests
+from bs4 import BeautifulSoup
+
+
+# Set the technology and version to search for
+technology = "nginx"
+version = "1.17"
+
+# Set the URL for the NVD website
+nvd_url = "https://nvd.nist.gov/vuln/search"
+
+# Set the parameters for the search
+params = {
+    "adv_search": "true",
+    "cpe_version": f"cpe:2.3:a:{technology}:{version}:*",
+    "startIndex": 0,
+    "resultsPerPage": 50,
+}
+
+# Send a GET request to the NVD website
+response = requests.get(nvd_url, params=params)
+
+# Parse the HTML response
+soup = BeautifulSoup(response.text, "html.parser")
+
+# Get the table with the CVEs
+cve_table = soup.find("table", id="vuln-list-table")
+
+# Initialize a list for the CVEs
+cves = []
+
+# Iterate over the rows in the table
+for row in cve_table.find_all("tr"):
+    # Get the columns in the row
+    columns = row.find_all("td")
+
+    # Skip rows without enough columns
+    if len(columns) < 5:
+        continue
+
+    # Get the CVE ID and description
+    cve_id = columns[0].text
+    description = columns[4].text
+
+    # Append the CVE to the list
+    cves.append({"cve_id": cve_id, "description": description})
+
+# Save the CVEs to a JSON file
+with open("cves.json", "w") as f:
+    json.dump(cves, f)
